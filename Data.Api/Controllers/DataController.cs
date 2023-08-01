93fdc488613d29b44a93e8ff200023c21b357da1
procodesnet <www.procodes.net@gmail.com>
2023-08-01 13:34:07 +0200

    Add get all controller

---
+++ Data.Api/Controllers/DataController.cs
@@ -0,0 +1,30 @@
+﻿using Data.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Data.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DataController : ControllerBase
+	{
+		private readonly IDataRepository dataRepository;
+
+		public DataController(IDataRepository dataRepository)
+		{
+			this.dataRepository = dataRepository;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult> GetRecords()
+		{
+			try
+			{
+				return Ok(await dataRepository.GetRecords());
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, "Error retriving data from the database.");
+			}
+		}
+	}
+}
